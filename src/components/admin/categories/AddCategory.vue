f6fa11dbf7eed73c8c30396896895b39e7717f13
aurora43 <obadahalak538@gmail.com>
2023-07-23 16:47:00 +0300

    refactor categoies

--- src/components/admin/categories/AddCategory.vue
+++ src/components/admin/categories/AddCategory.vue
@@ -3,7 +3,7 @@ import { useCategory } from "../../../stores/admin/category";
 import { useCompany } from "../../../stores/admin/company";
 
 import addForm from "../../base/AddForm.vue";
-import { isEmpty } from '@/composables/isEmpty.js'
+import BaseInput from "../../base/BaseInput.vue";
 import { ref } from "vue";
 const category = useCategory();
 const companyStore = useCompany();
@@ -15,16 +15,14 @@ let company_id = ref();
 
 function save() {
 
-    let data = new FormData();
-    if (!isEmpty(name.value) && !isEmpty(company_id.value)) {
+    category.store({
+        'name': name.value,
+        'company_id': company_id.value,
+    });
+    document.getElementById('form').reset();
 
-        data.append('name', name.value);
-        data.append('company_id', company_id.value);
-
-        category.store(data);
-        document.getElementById('form').reset();
-    }
 }
+
 </script>
 <template>
     <div class="">
@@ -32,22 +30,18 @@ function save() {
             <template #inputs>
                 <form id="form" @submit.prevent="save">
                     <div class="my-10">
-                        <p>catgory name:</p>
-                    <input class="mt-2 w-full rounded-md " type="text" v-model="name" placeholder="enter company name">
-                </div>
-                <div class="my-10">
-                    <p> select Category: </p>
-                    <select class="mt-2 w-full rounded-md" v-model="company_id">
-                        <option class="p-2 bg-slate-50 border-2 " :value="company.id" v-for="company in companyStore.data">
-                           <span >  {{ company.name }}</span>
-                        </option>
-                    </select>
-                </div>
+                        <BaseInput :type="'input'" v-model="name" :label="'enter company name'" />
 
-                <div>
-                    <button type="submit" class="p-2 w-full bg-gray-900 text-white rounded-md">Save</button>
-                </div>
-            </form>
+                    </div>
+                    <div class="my-10">
+                        <BaseInput :type="'select'" v-model="company_id" :label="'select Company:'"
+                            :data="companyStore.data" />
+                    </div>
+
+                    <div>
+                        <button type="submit" class="p-2 w-full bg-gray-900 text-white rounded-md">Save</button>
+                    </div>
+                </form>
             </template>
         </addForm>
 
--- src/components/admin/companies/AddCompany.vue
+++ src/components/admin/companies/AddCompany.vue
@@ -1,51 +1,49 @@
 <script setup>
 import { useCompany } from "../../../stores/admin/company";
 import addForm from "../../base/AddForm.vue";
+import BaseInput from '../../base/BaseInput.vue'
 import { ref } from "vue";
-import { isEmpty } from '@/composables/isEmpty.js'
 
 const company = useCompany();
 
 let image = ref();
 let name = ref();
+
 function uploadImage(event) {
 
     image.value = event.target.files[0];
 
 }
 function save() {
-    let data = new FormData();
-    
-    
-    if (!isEmpty(name.value) && !isEmpty(image.value)) {
-        
-        data.append('name', name.value);
-        data.append('logo', image.value);
-        company.store(data);
-        document.getElementById('form').reset();  
-    }
-    
+
+    company.store({
+        'name': name.value,
+        'logo': image.value,
+    });
+
+    document.getElementById('form').reset();
+
 }
 </script>
 <template>
     <div class="">
         <addForm :store="useCompany()" mode="company">
             <template #inputs>
-                <form id="form"  @submit.prevent="save">
+                <form id="form" @submit.prevent="save">
 
                     <div class="my-10">
-                        <p>company name:</p>
-                    <input class="mt-2 w-full rounded-md " type="text" v-model="name"
-                        placeholder="enter company name">
-                </div>
-                <div class="my-10">
-                    <p>company image:</p>
-                    <input class="p-2 rounded-md w-1/2" id="image" @change="uploadImage" type="file">
-                </div>
 
-                <div>
-                    <button type="submit"  class="p-2 w-full bg-gray-900 text-white rounded-md">Save</button>
-                </div>
+                        <BaseInput :type="'input'" type="text" :label="'company name:'" v-model="name" />
+
+                    </div>
+                    <div class="my-10">
+                        <p>company image:</p>
+                        <input class="p-2 rounded-md w-1/2" id="image" @change="uploadImage" type="file">
+                    </div>
+
+                    <div>
+                        <button type="submit" class="p-2 w-full bg-gray-900 text-white rounded-md">Save</button>
+                    </div>
                 </form>
             </template>
         </addForm>
