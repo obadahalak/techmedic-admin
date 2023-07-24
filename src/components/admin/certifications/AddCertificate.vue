c7c9d115bdddd09ab5bd7e8b7d101e4a26c58001
aurora43 <obadahalak538@gmail.com>
2023-07-24 12:03:45 +0300

    refactor Certifaction

--- src/components/admin/certifications/AddCertificate.vue
+++ src/components/admin/certifications/AddCertificate.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { useCertificate } from "../../../stores/admin/certificate";
 import addForm from "../../base/AddForm.vue";
+import BaseInput from "../../base/BaseInput.vue";
 import { ref } from "vue";
-import { isEmpty } from '@/composables/isEmpty.js'
 const certificate = useCertificate();
 
 let image = ref();
@@ -16,40 +16,38 @@ function uploadImage(event) {
 }
 
 function save() {
-    let data = new FormData();
 
-    if (!isEmpty(title.value) && !isEmpty(image.value ) && !isEmpty(description.value )) {
-        data.append('title', title.value);
-        data.append('image', image.value);
-        data.append('description', description.value);
-        certificate.store(data);
-        document.getElementById('form').reset();
-    }
+    certificate.store({
+        'title': title.value,
+        'image': image.value,
+        'description': description.value,
+    });
+    document.getElementById('form').reset();
 }
 </script>
 <template>
     <div class="">
         <addForm :store="certificate" mode="certificate">
             <template #inputs>
-                <form id="form" @submit.prevent="save" >
+                <form id="form" @submit.prevent="save">
                     <div class="my-10">
-                        <p> title:</p>
-                        <input class="mt-2 w-full rounded-md " type="text" v-model="title" placeholder="enter  name">
-                </div>
-                <div class="my-10">
-                    <p> descrption:</p>
-                    <input class="mt-2 w-full rounded-md " type="text" v-model="description"
-                        placeholder="enter descrption ">
-                </div>
-                <div class="my-10">
-                    <p> image:</p>
-                    <input class="p-2 rounded-md w-1/2" id="image" @change="uploadImage" type="file">
-                </div>
 
-                <div>
-                    <button type="submit" class="p-2 w-full bg-gray-900 text-white rounded-md">Save</button>
-                </div>
-            </form>
+                        <BaseInput :type="'input'" v-model="title" :label="'enter the title'" />
+
+                    </div>
+                    <div class="my-10">
+                        <BaseInput :type="'input'" v-model="description" :label="'enter the descrption'" />
+
+                    </div>
+                    <div class="my-10">
+                        <p> image:</p>
+                        <input class="p-2 rounded-md w-1/2" id="image" @change="uploadImage" type="file">
+                    </div>
+
+                    <div>
+                        <button type="submit" class="p-2 w-full bg-gray-900 text-white rounded-md">Save</button>
+                    </div>
+                </form>
             </template>
         </addForm>
 
